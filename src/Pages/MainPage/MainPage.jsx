28b83cf613f558e0c156980a90f34cf7f6a1d423
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-11-22 07:44:09 +0200

    deploy test

--- src/Pages/MainPage/MainPage.jsx
+++ src/Pages/MainPage/MainPage.jsx
@@ -1,9 +1,11 @@
 import Header from "../../Components/Header/Header";
+import Swiper from "../../Components/Swiper/Swiper";
 
 const MainPage = () => {
   return (
     <div>
       <Header />
+      <Swiper />
     </div>
   );
 };
