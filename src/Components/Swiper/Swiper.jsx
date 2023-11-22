f8ae214982455be9c78b81cbb26e81d09990be6a
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-11-22 10:17:32 +0200

    add: video

--- src/Components/Swiper/Swiper.jsx
+++ src/Components/Swiper/Swiper.jsx
@@ -6,6 +6,24 @@ const Swiper = () => {
       <video className={style.container_video} autoPlay loop muted playsInline>
         <source src="./assets/video.mp4" type="video/mp4" />
       </video>
+      <div className={style.container_text}>
+        <h1 className={style.container_text__h1}>
+          Грай у улюблені<br></br>
+          <span className={style.container_text__span}>
+            шутери, стратегії, гонки
+          </span>
+          <br></br>в хмарі
+        </h1>
+        <p className={style.container_text__p}>
+          Топові ігри на <span>будь-якому ПК</span> вже зараз, реєструйся і грай
+          30 хв безкоштовно
+        </p>
+        <div>
+          <button className={style.container_text__firstButton}>
+            ПОЧАТИ ГРАТИ
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
