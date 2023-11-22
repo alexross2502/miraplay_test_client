18137bade411470065bc728fa57da78f4d0ceae4
alexross2502 <93192857+alexross2502@users.noreply.github.com>
2023-11-22 08:26:10 +0200

    deploy test2

--- src/Components/Swiper/Swiper.jsx
+++ src/Components/Swiper/Swiper.jsx
@@ -3,15 +3,8 @@ import style from "./swiper.module.css";
 const Swiper = () => {
   return (
     <div className={style.container}>
-      <video
-        src="./media/video.mp4"
-        type="video/mp4"
-        className={style.container_video}
-        loop
-        autoPlay
-      ></video>
       <video className={style.container_video} autoPlay loop muted playsInline>
-        <source src="./media/video.mp4" type="video/mp4" />
+        <source src="./assets/video.mp4" type="video/mp4" />
       </video>
     </div>
   );
